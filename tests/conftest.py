e3734e3befbedd9bafaa6b7df031b0b35f994c85
Giovanni <piombonicoligiovanni@gmail.com>
2026-03-17 17:41:55 +0100

    Added GitHub Actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,21 +3,18 @@ import sys
 import os
 import pytest
 
-# 1. RISOLUZIONE DEL PATH
-# Calcola il percorso assoluto della cartella 'core' e lo aggiunge a sys.path
+#Path Resolving
 core_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../core'))
 if core_path not in sys.path:
     sys.path.insert(0, core_path)
 
 
-# 2. FIXTURE CONDIVISE (Opzionale ma utilissimo)
-# Importiamo il modulo DOPO aver sistemato il path
+# Necessary fixture to reset the FX cache before each test, ensuring that tests do not interfere with each other due to cached data.
 import portfolio
 
 @pytest.fixture(autouse=True)
 def reset_fx_cache():
     """
-    Questa fixture pulisce la cache FX prima di OGNI test in tutta la suite.
-    Essendo in conftest.py con autouse=True, non devi scriverla nei file di test.
+    This fixture is automatically applied to all tests. It clears the FX cache before each test runs, ensuring that tests do not interfere with each other due to cached data.
     """
     portfolio.fx_cache.clear()
