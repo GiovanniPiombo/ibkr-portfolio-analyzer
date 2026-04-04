92c663360f14287fd942cab4c444c546bcbdba23
Giovanni <piombonicoligiovanni@gmail.com>
2026-04-04 15:01:07 +0200

    fix(logger): update logger name and log file to match project name

--- core/logger.py
+++ core/logger.py
@@ -22,9 +22,9 @@ def setup_logger():
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
-    log_file = os.path.join(log_dir, "ibkr_analyzer.log")
+    log_file = os.path.join(log_dir, "AlphaPaths.log")
 
-    logger = logging.getLogger("IBKR ANALYZER")
+    logger = logging.getLogger("AlphaPaths")
     logger.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter(
